d7dc68eccccd338ae03eea38ccac4fdfd2ad83f5
nghia1991 <dnghia13@gmail.com>
2016-10-08 17:16:52 +0700

    upload project

---
+++ public/javascripts/script.js
@@ -0,0 +1,86 @@
+
+function request(url, method, data, done, fail) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if (done != null) done(this.responseText);
+        } else {
+            if (fail != null) fail();
+        }
+    };
+    xhttp.open(method, url, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(data);
+}
+
+var isRequesting = false;
+
+function generateInfoClick() {
+
+    if (isRequesting) return true;
+    isRequesting = true;
+
+    document.getElementById("result").className = "hidden";
+    var youtubeUrl = document.getElementById('youtubeUrlInput').value;
+    if (youtubeUrl.indexOf('https://www.youtube.com') != 0 && youtubeUrl.indexOf('https://youtube.com') != 0) {
+        document.getElementById('youtubeUrlInput').className = document.getElementById('youtubeUrlInput').className + " input-invalid";
+        isRequesting = false;
+        return;
+    } else {
+        document.getElementById('youtubeUrlInput').className = document.getElementById('youtubeUrlInput').className.replace(' input-invalid', '');
+    }
+
+
+    document.getElementById('imgLoading').className = '';
+  
+    request('/ydl', 'POST', 'youtubeUrl=' + youtubeUrl, function(res){
+
+        isRequesting = false;
+
+        if (res == 'error') {
+            isRequesting = false;
+            document.getElementById('cantGetVideo').className = document.getElementById('cantGetVideo').className.replace(' hidden', '');
+            return;
+        }
+        if (document.getElementById('cantGetVideo').className.indexOf('hidden') == -1) document.getElementById('cantGetVideo').className = document.getElementById('cantGetVideo').className + ' hidden';
+        document.getElementById("result").className = "";
+        var obj = JSON.parse(res);
+        document.getElementById('videoInfo').value = 'Title: ' + obj.title + '\nAuthor: ' + obj.author + '\nView count: ' + obj.view_count;
+        document.getElementById('videoImg').src = obj.img;
+        var links = obj.links;
+        var selectHtml = '';
+        var qualityDuplicate = {};
+        var hiddenInputHtml = '';
+
+        for (var i = 0, l = links.length; i < l; i++) {
+            if (qualityDuplicate[links[i].quality] == null) {
+                selectHtml += '<option value="' + i + '">' + links[i].quality  +'</option>';
+                qualityDuplicate[links[i].quality] = 1;
+            } else {
+                selectHtml += '<option value="' + i + '">' + links[i].quality + '_' + qualityDuplicate[links[i].quality]  +'</option>';
+                qualityDuplicate[links[i].quality]++;
+            }
+            hiddenInputHtml += '<input type="hidden" id="hiddenLink_' + i + '" value="' +  links[i].url  + '" />';
+        }
+ 
+        document.getElementById('qualitySelect').innerHTML = selectHtml;
+        document.getElementById('resultHidden').innerHTML = hiddenInputHtml;
+
+        document.getElementById('downloadA').href = links[0].url;
+        document.getElementById('downloadA').download = "";
+
+        document.getElementById('imgLoading').className = 'hidden';
+
+    }, function() {
+        document.getElementById("result").className = "hidden";
+        document.getElementById('imgLoading').className = 'hidden';
+        isRequesting = false;
+    });
+}
+
+function qualitySelectChange() {
+    var quality =  document.getElementById('qualitySelect').value;
+    var url = document.getElementById('hiddenLink_' + quality).value;  
+    document.getElementById('downloadA').href = url;
+    document.getElementById('downloadA').download = "";
+}
---
+++ routes/youtube_downloader.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+
+    request(req.body.youtubeUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(youtubeHtmlParser(body));
+        } else {
+            res.send('error');
+        }
+    });
+
+});
+
+function youtubeHtmlParser(html){
+
+    // between 'ytplayer.config =' and `;ytplayer.load = function()`
+    var rawJson = html.match(/(?=ytplayer.config =).*?(?=;ytplayer.load = function())/);
+  
+    if (rawJson == null || rawJson[0] == null) {
+        return 'error';
+    }
+
+    try {
+        var objectJson = JSON.parse(rawJson[0].substring('ytplayer.config ='.length));
+        var args = objectJson.args;
+        var video = {};
+
+        video.author = args.author;
+        video.title = args.title;
+        video.view_count = args.view_count;
+        video.img = args.iurlmq.replace(/\\/g, '');
+        video.links = [];
+        var streamMaps = args.url_encoded_fmt_stream_map.split(',');
+
+        for(var i = 0, l = streamMaps.length; i < l; i++) {
+            var temp = streamMaps[i].split('\u0026');
+            var element = {};
+            for (var j = 0, l1 = temp.length; j < l1; j++) {
+                
+                if (temp[j].indexOf('quality=') > - 1 && element.quality == null) element.quality = temp[j].substring('quality='.length);
+                if (temp[j].indexOf('url=') > - 1 && element.url == null) {
+                    element.url = unescape(temp[j].substring('url='.length));
+                }
+            }
+            video.links.push(element);
+        }
+
+        return video;
+    } catch(e){
+        return 'error';
+    }
+    
+}
+
+module.exports = router;
