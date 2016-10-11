a1a0660101200e47b1fcee031fa3c383d6066d9a
Nghia <dnghia13@gmail.com>
2016-10-11 09:58:56 +0700

    add GA

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -32,7 +32,12 @@ function generateInfoClick() {
 
 
     document.getElementById('imgLoading').className = '';
-  
+    
+    ga('send', 'event', {
+        eventCategory: 'generateInfoClick',
+        eventAction: 'click'
+    });
+
     request('/ydl', 'POST', 'youtubeUrl=' + youtubeUrl, function(res){
 
         isRequesting = false;
@@ -104,5 +109,14 @@ function urlInputPaste() {
     setTimeout(function() {
         document.getElementById("generateInfo").click();
     }, 200);
-
 }
+
+// Google Analytic
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-85518523-1', 'auto');
+ga('send', 'pageview');
+
